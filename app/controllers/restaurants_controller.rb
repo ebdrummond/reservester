ce971c19d0eaea52fcbbdab74cadea160b2662b9
erindrummond <e.b.drummond@gmail.com>
2014-03-30 23:00:35 -0500

    Implement controller actions & views for restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,59 @@
+class RestaurantsController < ApplicationController
+  before_filter :assign_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def assign_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :description,
+      :street_1,
+      :street_2,
+      :city,
+      :state,
+      :zip,
+      :phone
+    )
+  end
+end
